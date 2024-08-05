545dbfb3735aab6dbc59b4bc60c913500719caa6
Chen Shuo <1342662728@qq.com>
2024-08-05 18:17:02 +0800

    fix: change name

--- packages/commitlint/__tests__/commitlint.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const commitlint = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(commitlint(), 'Hello from commitlint');
-console.info('commitlint tests passed');
--- packages/markdownlint/__tests__/markdownlint.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const markdownlint = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(markdownlint(), 'Hello from markdownlint');
-console.info('markdownlint tests passed');
